c783058656cf152c7ef091d6f8cc5ec95b8357ed
Maia Iyer <maia.raj.iyer@gmail.com>
2025-06-18 16:29:56 -0400

    testing environment variables
    Signed-off-by: Maia Iyer <maia.raj.iyer@gmail.com>

--- acp/acp_weather_service/src/acp_weather_service/configuration.py
+++ acp/acp_weather_service/src/acp_weather_service/configuration.py
@@ -1,7 +1,8 @@
 from pydantic_settings import BaseSettings
 
 class Configuration(BaseSettings):
-    llm_model: str = "llama3.1"
-    llm_api_base: str = "http://localhost:11434/v1"
-    llm_api_key: str = "dummy"
+    llm_model: str = os.getenv("LLM_MODEL", "llama3.1")
+    llm_api_base: str = os.getenv("LLM_API_BASE", "http://localhost:11434/v1")
+    llm_api_key: str = os.getenv("LLM_API_KEY", "dummy")
+    llm_headers: str = os.getenv("LLM_HEADERS", "")
 
